c22617665aea80214648513493d94fd164f87003
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-02 17:48:13 +0700

    IT WORKS YAYAYAY

--- rockytanker.lua
+++ rockytanker.lua
@@ -7,4 +7,5 @@ if rocky ~= "rockytanker" then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
 goofy ahh rockytanker
 webhook doesnt work :skull:
 okay another webhook test
+IT WORKS :longcrazyvn_thumbsup:
 ]]--
