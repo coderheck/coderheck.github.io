75de186cd23266537476aa22cde7b1c13316ed9f
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-02 17:42:44 +0700

    skull...

--- rockytanker.lua
+++ rockytanker.lua
@@ -4,4 +4,5 @@ local rocky = "rockytanker"
 if rocky == "rockytanker" then print(rocky) end
 if rocky ~= "rockytanker" then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
 goofy ahh rockytanker
+webhook doesnt work :skull:
 ]]--
