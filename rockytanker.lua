ad3ccc1e37f429e9c8d00d2676fa02c9ce1af8ce
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-02 17:40:29 +0700

    goofy rocky + webhook test

--- rockytanker.lua
+++ rockytanker.lua
@@ -3,4 +3,5 @@ local rocky = "rockytanker"
 
 if rocky == "rockytanker" then print(rocky) end
 if rocky ~= "rockytanker" then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
+goofy ahh rockytanker
 ]]--
