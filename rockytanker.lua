240ac9dd20e096e3b7fb064aa2c8e6265e4ec9fe
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-12 22:35:57 +0700

    death

--- rockytanker.lua
+++ rockytanker.lua
@@ -1,4 +1,4 @@
---[[
+
 local rocky = "rockytanker"
 
 local random = math.random(1, 3)
@@ -6,8 +6,7 @@ if random == 1 then print(rocky) end
 if random == 2 then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
 if random == 3 then print("the RACISM in my blood...")
 
-goofy ahh rockytanker
-webhook doesnt work :skull:
-okay another webhook test
-IT WORKS :longcrazyvn_thumbsup:
-]]--
+--goofy ahh rockytanker
+--webhook doesnt work :skull:
+--okay another webhook test
+--IT WORKS :longcrazyvn_thumbsup:
