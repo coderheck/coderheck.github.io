6212458df71a6e2f6a2c3e8bb62ec75120a4c176
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-12 22:42:01 +0700

    fail

--- rockytanker.lua
+++ rockytanker.lua
@@ -4,7 +4,7 @@ local rocky = "rockytanker"
 local random = math.random(1, 3)
 if random == 1 then print(rocky) end
 if random == 2 then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
-if random == 3 then print("the RACISM in my blood...")
+if random == 3 then print("the RACISM in my blood...") end
 
 --goofy ahh rockytanker
 --webhook doesnt work :skull:
