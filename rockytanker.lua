b3b93efb7992cb1ef75e78223aee8c97ac3b082b
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-15 23:30:37 +0700

    change

--- rockytanker.lua
+++ rockytanker.lua
@@ -1,10 +1,11 @@
-
+-- dummy script
 local rocky = "rockytanker"
 
 local random = math.random(1, 3)
-if random == 1 then print(rocky) end
-if random == 2 then print("ROCKY IS ROCKY GRAAAAAAAHHHHHH") end
-if random == 3 then print("the RACISM in my blood...") end
+print(random)
+print(rocky)
+print("goofy ahh rocky")
+print("embot is racist, noted")
 
 --goofy ahh rockytanker
 --webhook doesnt work :skull:
