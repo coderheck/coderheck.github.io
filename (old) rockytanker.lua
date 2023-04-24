2554954c5cc02cdaf73a824c7a95fca6a4246bf2
coderheck <loxzyioxzybusiness@hotmail.com>
2023-04-24 23:41:17 +0700

    2 rockytankers

---
+++ (old) rockytanker.lua
@@ -0,0 +1,16 @@
+-- dummy script
+local rocky = "rockytanker"
+
+local random = math.random(1, 3)
+print(random)
+print(rocky)
+print("goofy ahh rocky")
+print("embot is racist, noted")
+
+local pi = math.pi
+print("the digits of pi are " .. pi)
+
+--goofy ahh rockytanker
+--webhook doesnt work :skull:
+--okay another webhook test
+--IT WORKS :longcrazyvn_thumbsup:
--- rockytanker.lua
+++ rockytanker.lua
@@ -1,6 +1,19 @@
--- dummy script
-local rocky = "rockytanker"
+-- Copyright (C) 2023 Tria
+-- This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+-- If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+--- @class rockytanker
+--- This is the documentation on rockytanker, one of the documentation writers.
+
+local rockytanker: Types.rockytanker = {}
+rockytanker.__index = rockytanker
+local rockyt = "the racist god."
+
+--- @param stringToWrite string
+--- @within rockytanker
+function rockytanker:Write(stringToWrite: string)
+    if stringToWrite ~= nil then print(stringToWrite) else return nil
+end
 local random = math.random(1, 3)
 print(random)
 print(rocky)
@@ -14,3 +27,4 @@ print("the digits of pi are " .. pi)
 --webhook doesnt work :skull:
 --okay another webhook test
 --IT WORKS :longcrazyvn_thumbsup:
+end
